e7720b90e3285ebee3ff8c52013fa20e696195a2
dhakshathapm-crypto <dhakshathapm@gmail.com>
2026-06-24 16:19:15 +0530

    Create vowel_counter.py

---
+++ vowel_counter.py
@@ -0,0 +1,8 @@
+word = "hello"
+count = 0
+
+for letter in word:
+    if letter == "a" or letter == "e" or letter == "i" or letter == "o" or letter == "u":
+        count = count + 1
+
+print(count)
