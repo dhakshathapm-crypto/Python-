4a3e6d6562b19d640f9f5127cc0fb47a3a273c06
dhakshathapm-crypto <dhakshathapm@gmail.com>
2026-06-30 09:51:35 +0530

    Delete palindrome.checker.py

--- palindrome.checker.py
+++
@@ -1,9 +0,0 @@
-def is_palindrome(word):
-    reversed_word = word[::-1]
-    if reversed_word == word:
-        return True
-    else:
-        return False
-
-print(is_palindrome("madam"))
-print(is_palindrome("hello"))
